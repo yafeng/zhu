b05a7358577692970c2adcc90fc99e7ca58a5f79
Yafeng Zhu <yafengzhu@130-229-14-64-dhcp.wlan.ki.se>
2013-01-23 14:55:44 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='biostuff',
+    version='0.1dev',
+    author='yafeng',
+    author_email='yafeng.zhu@scilifelab.se',
+    url='http://www.github.com/yafeng/biostuff',
+    packages=['biostuff'],
+    license='GPLv3',
+    long_description=open('README.txt').read(),
+    )
